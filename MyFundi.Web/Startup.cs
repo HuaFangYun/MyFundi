77ad98e00ceddb54826a3503158cd9f7569ef689
Martin Alex Okello <martin.okello@gmail.com>
2022-03-09 16:45:51 +0000

    Update to MyFundi App Code base

--- MyFundi.Web/Startup.cs
+++ MyFundi.Web/Startup.cs
@@ -375,7 +375,7 @@ namespace MyFundi.Web
             {
                     // To learn more about options for serving an Angular SPA from ASP.NET Core,
                     // see https://go.microsoft.com/fwlink/?linkid=864501
-                    var path = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp");
                 spa.Options.SourcePath = path;
 
                 if (env.IsDevelopment())
